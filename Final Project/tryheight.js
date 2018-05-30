9f01953720f94ae8d392105c86d26834186c969d
Unknown <renaldy_fargo@yahoo.com>
2018-05-31 00:51:59 +0700

    Follow Camera !!!!
    Camera udah bisa ngikutin pesawatnya,
    heightmap.js itu bisa dibikin jadi recycle bin aja kali ya? wkwkwkwkkw

--- Final Project/tryheight.js
+++ Final Project/tryheight.js
@@ -7,76 +7,52 @@
         //create babylon engine
         engine = new BABYLON.Engine(canvas, true);
 
+
+        var createScene = function(){
         //create scene
-        scene = new BABYLON.Scene(engine);
+          scene = new BABYLON.Scene(engine); //play scene
 
-        scene.clearColor = new BABYLON.Color3(0.2, 0.5, 0.9);
-        scene.ambientColor = new BABYLON.Color3(0.3, 0.3, 0.3);
+          scene.clearColor = new BABYLON.Color3(0.2, 0.5, 0.9);
+          scene.ambientColor = new BABYLON.Color3(0.3, 0.3, 0.3);
 
-        scene.fogMode = BABYLON.Scene.FOGMODE_EXP2;
-        scene.fogDensity = 0.002;
-        scene.fogColor = new BABYLON.Color3(0.9,0.9,0.9);
+          scene.fogMode = BABYLON.Scene.FOGMODE_EXP2;
+          scene.fogDensity = 0.002;
+          scene.fogColor = new BABYLON.Color3(0.9,0.9,0.9);
 
-
-// Skybox
-var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
-var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-skyboxMaterial.backFaceCulling = false;
-skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("TropicalSunnyDay/TropicalSunnyDay", scene);
-skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-skybox.material = skyboxMaterial;
-
+          // Skybox
+          var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+          var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+          skyboxMaterial.backFaceCulling = false;
+          skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("TropicalSunnyDay/TropicalSunnyDay", scene);
+          skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+          skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+          skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+          skybox.material = skyboxMaterial;
 
 
-        //create a FreeCamera, and set its position to (x:0, y:4, z:-10)
-        // camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 50,100), scene);
+// Parameters: name, position, scene    
+var camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 10, -10), scene);
 
+//The goal distance of camera from target
+camera.radius = 30;
 
-        // camera.keysUp.push(38);
-        // camera.keysDown.push(40);
-        // camera.keysRight.push(39);
-        // camera.keysLeft.push(37);
+// The goal height of camera above local origin (centre) of target
+camera.heightOffset = 20;
 
+// The goal rotation of camera around local origin (centre) of target in x y plane
+camera.rotationOffset = 0;
 
-        camera = new BABYLON.ArcRotateCamera("Camera",0,0,10, new BABYLON.Vector3(0,0,0),scene);
+//Acceleration of camera in moving from current to goal position
+camera.cameraAcceleration = 0.005
 
-        //target the camera to scene origin
-        camera.setTarget(new BABYLON.Vector3(0,0,0));
-        camera.setPosition(new BABYLON.Vector3(0,50,100));
+//The speed at which acceleration is halted 
+camera.maxCameraSpeed = 10
 
-        var cameraPosX = 0;
-        var cameraPosY = 50;
-        var cameraPosZ = 100;
-        var cameraPosAdd = 1;
+// This attaches the camera to the canvas
+   camera.attachControl(canvas, true);
 
-        window.addEventListener('keydown',function(event){
-         if(event.keyCode == 39 ){
-           camera.setPosition(new BABYLON.Vector3(cameraPosX-cameraPosAdd,cameraPosY,cameraPosZ));
-           cameraPosX = cameraPosX-cameraPosAdd;
-         }
-         if(event.keyCode == 37 ){
-            camera.setPosition(new BABYLON.Vector3(cameraPosX+cameraPosAdd,cameraPosY,cameraPosZ));
-            cameraPosX = cameraPosX+cameraPosAdd;
-         }
-         if(event.keyCode == 38 ){
-            camera.setPosition(new BABYLON.Vector3(cameraPosX,cameraPosY,cameraPosZ+cameraPosAdd));
-            cameraPosZ = cameraPosZ-cameraPosAdd;
-         }
-         if(event.keyCode == 40 ){
-            camera.setPosition(new BABYLON.Vector3(cameraPosX,cameraPosY,cameraPosZ-cameraPosAdd));
-            cameraPosZ = cameraPosZ+cameraPosAdd;            
-         }
-        })
-
-        // attach the camera to the canvas
-       camera.attachControl(canvas,true);
-
-
-
-
-
+// NOTE:: SET CAMERA TARGET AFTER THE TARGET'S CREATION AND NOTE CHANGE FROM BABYLONJS V 2.5
+//targetMesh created here
 
 
         // create a basic light, aiming 0,8,0
@@ -97,6 +73,8 @@ skybox.material = skyboxMaterial;
       BABYLON.SceneLoader.ImportMesh("","","airplane1.babylon", scene, function(newMeshes){
         airplane = newMeshes[0];
         setup(airplane);
+        camera.lockedTarget = airplane; //version 2.5 onwards
+
       });
 
 
@@ -107,11 +85,27 @@ skybox.material = skyboxMaterial;
         box.material = material;
 
 
+        return scene;
+        }
+
+        var scene = createScene();
+
        engine.runRenderLoop(function() {
             scene.render();
         });
     });
     
+
+      function setupStart(mesh){
+        meshX = 0;
+        meshY = 0;
+        meshZ = 50;
+        meshAdd = 1;
+
+        mesh.scaling = new BABYLON.Vector3(1,1/16,1/8);
+        mesh.position = new BABYLON.Vector3(meshX, meshY, meshZ);
+      }
+
 
       function setup(mesh){
         meshX = 0;
